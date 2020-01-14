0d29164369cc5e2d5961eb11f3a0a9ce33459bf6
zachmolony <zachmolony@deloitte.co.uk>
2020-01-14 14:28:43 +0000

    added tcs

--- test/coolculator.test.js
+++ test/coolculator.test.js
@@ -11,15 +11,44 @@ describe('Coolculator', function() {
     expect(result).to.equal(5)
   })
 
-  // Uncomment this 👇
-  // it('should multiply', function() {
-  //   result = mm.multiply(2, 3)
-  //   expect(result).to.equal(6)
-  // })
+  it('should multiply', function() {
+    result = mm.multiply(2, 3)
+    expect(result).to.equal(6)
+  })
 
-  // Uncomment this 👇
-  // it('should subtract', function() {
-  //   result = mm.subtract(3, 2)
-  //   expect(result).to.equal(1)
-  // })
+  it('should subtract', function() {
+    result = mm.subtract(3, 2)
+    expect(result).to.equal(1)
+  })
+
+  it('should check cool', function() {
+    result = mm.isCool(2232)
+    expect(result).to.true
+  })
+
+  it('should sum', function() {
+    result = mm.doSum([2, 4, 2, 3])
+    expect(result).to.equal(11)
+  })
+
+  it('should multiply array', function() {
+    result = mm.multiplyArray([2, 4, 6, 8], 2)
+    expect(result).to.deep.equal([4, 8, 12, 16])
+  })
+
+  it('should find mean', function() {
+    result = mm.mean([2, 4, 6])
+    expect(result).to.equal(4)
+  })
+
+  it('should find factorial', function() {
+    result = mm.factorial(5)
+    expect(result).to.equal(120)
+  })
+
+  it('should give random', function() {
+    result = mm.random(5)
+    expect(result).to.equal(120)
+  })
+
 })
